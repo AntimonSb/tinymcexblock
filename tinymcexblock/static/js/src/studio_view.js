04e4bd8dd75f789de26993579642657184d52f27
AntimonSb <ante@extensionengine.com>
2016-09-26 10:32:16 -0400

    Missing spellchecker plugin on server

--- tinymcexblock/static/js/src/studio_view.js
+++ tinymcexblock/static/js/src/studio_view.js
@@ -34,7 +34,7 @@ function StudioEditSubmit(runtime, element) {
     selector: 'textarea',
     height: 500,
     plugins: [
-      'codemirror image link media paste spellchecker table textcolor'
+      'codemirror image link media paste table textcolor'
     ],
     toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
     content_css: '//www.tinymce.com/css/codepen.min.css'
